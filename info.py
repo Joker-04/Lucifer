b8c90db3943c78ee90ba4f7cc36e5b944d78cec1
Joker-04 <155981494+Joker-04@users.noreply.github.com>
2024-12-20 22:59:05 +0530

    Update info.py

--- info.py
+++ info.py
@@ -42,7 +42,7 @@ reqst_channel = environ.get('REQST_CHANNEL_ID', '') # request channel id ( make 
 
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://jayjoshi8275:yIxKNuuvhpDMewFq@cluster0.e2nug.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_URI = environ.get('DATABASE_URI', "")
 DATABASE_URI2 = environ.get('DATABASE_URI2', "")
 DATABASE_NAME = environ.get('DATABASE_NAME', "cluster")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Deendayal_files')
